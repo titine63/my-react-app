37ae0e1c2bebdafda45401b6cba78037bcd4d9f9
Sandrine <sblanchonnet@yahoo.fr>
2024-01-17 11:58:52 +0100

    components.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,34 @@
+//PokemonCard.tsx
+
 import React from 'react';
 
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 const PokemonCard = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const pokemon = pokemonList[1];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
+
 
 export default PokemonCard;
 
