9fd5d268ee3baa5821e1f7c1739580d15fe8dfcc
Sandrine <sblanchonnet@yahoo.fr>
2024-01-18 10:17:45 +0100

    props.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,12 @@
 //PokemonCard.tsx
-
+import PropTypes from "prop-types";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const PokemonCard = (props : any) => {
     console.log(props);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const {pokemon} = props;
+
+    
     return (
         <figure>
             {pokemon.imgSrc ? (
@@ -15,7 +17,15 @@ const PokemonCard = (props : any) => {
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
-}
+};
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string
+    }).isRequired
+};
+
 
 
 export default PokemonCard;
