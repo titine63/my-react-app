95e3aae35ba9856002e2c79a575df65f4c23875e
Sandrine <sblanchonnet@yahoo.fr>
2024-01-18 22:04:12 +0100

    state.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,54 @@
 
 import './App.css'
 import PokemonCard from './components/PokemonCard';
+import { useState} from 'react';
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
+  // Création de l'état pokemonIndex
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  // Fonction pour aller au Pokémon précédent
+  const goToPrevious = () => {
+    setPokemonIndex(prevIndex => prevIndex > 0 ? prevIndex - 1 : 0);
+  };
+
+  // Fonction pour aller au Pokémon suivant
+  const goToNext = () => {
+    setPokemonIndex(prevIndex => prevIndex < pokemonList.length - 1 ? prevIndex + 1 : prevIndex);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
+    {pokemonIndex > 0 && <button onClick={goToPrevious}>Précédent</button>}
+    <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+    {pokemonIndex < pokemonList.length - 1 && <button onClick={goToNext}>Suivant</button>}
+  </div>
   );
 }
 
